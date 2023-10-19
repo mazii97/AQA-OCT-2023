77bee4446141fed739c1dc28e26315287d2e0d3b
maziyoleksandr <mazijs2.sm@gmail.com>
2023-10-19 15:40:44 +0300

    Maziy Oleksandr

--- src/java/main/org/prog/poly/HugeTruck.java
+++ src/java/main/org/prog/poly/HugeTruck.java
@@ -1,5 +1,15 @@
 package org.prog.poly;
 
 public class HugeTruck extends GasTruck {
+    @Override
+    public void accelerate() {
+        System.out.println("Gas truck accelerates");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Gas truck slows down");
+    }
+
     //TODO: override accelerate and stop methods
 }
